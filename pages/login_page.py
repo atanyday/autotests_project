caee78896fac9b184d85f01d46028e2380946c51
Anna Bezrukova <annelia_55@mail.ru>
2023-04-05 14:44:41 +0300

    Autorization added

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,10 +6,41 @@ from selenium.webdriver.support import expected_conditions as EC
 from base.base_class import Base
 
 class Login_page(Base):
-    url = ""
+    url = "https://www.dinozavrik.ru/"
     def __int__(self, driver):
         super.__init__(driver)
 
+    # Locators
+    login_icon = "//a[@class='header__login']"
+    mail_field = "//input[@name='USER_LOGIN']"
+    password_field = "//input[@class='password']"
+    login_button = "//button[@name='Login']"
+
+    # Getters
+    def get_login_icon(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.login_icon)))
+    def get_mail_field(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.mail_field)))
+    def get_password_field(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.password_field)))
+    def get_login_button(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.login_button)))
+
+    # Actions
+    def click_login_icon(self):
+        self.get_login_icon().click()
+        print("Click login icon")
+    def input_mail(self):
+        mail = "qatest_mailbox@mail.ru"
+        self.get_mail_field().send_keys(mail)
+        print("Input user name")
+    def input_password(self):
+        password = "qRX2oxm7W4B2obD"
+        self.get_password_field().send_keys(password)
+        print("Input password")
+    def click_login_button(self):
+        self.get_login_button().click()
+        print("Click login button")
 
     # Methods
     def autorization(self):
@@ -18,3 +49,8 @@ class Login_page(Base):
         self.driver.set_window_size(1900, 1200)
         print("Browser opened")
         self.get_current_url()
+        self.click_login_icon()
+        self.input_mail()
+        self.input_password()
+        self.click_login_button()
+        print("Autorization success")
